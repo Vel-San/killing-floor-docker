93f501f604ee3898ad1e7fa62fbd3e1eeb4dd2e2
Vel-San <13340023+Vel-San@users.noreply.github.com>
2020-06-02 23:34:18 +0200

    [release] v3.1 Public Release
    - [removed] KFMaxPlayers, CleanAppID & ServerColor

--- kf1_functions.sh
+++ kf1_functions.sh
@@ -69,10 +69,6 @@ function load_config() {
     sed -i "s,RedirectToURL=,RedirectToURL=$KF_REDIRECT,g" KillingFloor.ini
     
     ############# MUTATORS RELATED REPLACEMENTS #############
-    # If you have MaxPlayers or ServerColor enabled, uncomment the 2 below commands
-    # And don't forget to edit the config of these 2 mutators in the /System directory
-    # sed -i '/ServerActors=UWeb.WebServer/a ServerActors=ServerColor.ServerColorActor' KillingFloor.ini
-    # sed -i '/ServerActors=UWeb.WebServer/a ServerActors=KFMaxPlayers.KFMaxPlayers' KillingFloor.ini
     # If you are using MapVote Handler Fix, uncomment the below command, make sure you already have the pre-configured
     # map votings in the /System
     sed -i "s/VotingHandlerType=xVoting.xVotingHandler/VotingHandlerType=KFMapVoteV2.KFVotingHandler/g" KillingFloor.ini
