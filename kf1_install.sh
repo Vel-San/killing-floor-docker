928befdd93282df0a6fbcb0a30e3a51a9976da71
Vel <13340023+Vel-San@users.noreply.github.com>
2021-08-21 20:34:58 +0200

    [misc] Added echos to track script progress
    - Fixed indentation

--- kf1_install.sh
+++ kf1_install.sh
@@ -1,34 +1,37 @@
+#!/bin/bash
+
 set -o xtrace
 
 if [[ ! -f "/home/steam/servers/kf/System/Core.u" ]]; then
-    echo "No server install found; preparing to run steamcmd..."
+  echo "#### No server install found; preparing to run steamcmd... ####"
 
-    if [[ -z $STEAM_USER ]]; then
-        echo "Env var STEAM_USER is required but not found"
-        exit 1
-    fi
+  if [[ -z $STEAM_USER ]]; then
+    echo "## Env var STEAM_USER is required but not found ##"
+    exit 1
+  fi
 
-    if [[ -z $STEAM_PASSWORD ]]; then
-        echo "Env var STEAM_PASSWORD is required but not found"
-        exit 1
-    fi
+  if [[ -z $STEAM_PASSWORD ]]; then
+    echo "## Env var STEAM_PASSWORD is required but not found ##"
+    exit 1
+  fi
 
-    if [[ -z $STEAM_CODE ]]; then
-        echo "Env var STEAM_CODE not found; this is only a problem if steam guard is enabled"
-    fi
+  if [[ -z $STEAM_CODE ]]; then
+    echo "## Env var STEAM_CODE not found; this is only a problem if steam guard is enabled ##"
+  fi
 
-    /home/steam/steamcmd/steamcmd.sh \
-        +login $STEAM_USER $STEAM_PASSWORD $STEAM_CODE \
-        +force_install_dir /home/steam/servers/kf \
-        +app_update 215360 validate \
-        +quit
+  /home/steam/steamcmd/steamcmd.sh \
+    +login $STEAM_USER $STEAM_PASSWORD $STEAM_CODE \
+    +force_install_dir /home/steam/servers/kf \
+    +app_update 215360 validate \
+    +quit
 
-    if [[ $? -ne 0 ]]; then
-        echo "Install failed, aborting..."
-        exit 1
-    fi
+  if [[ $? -ne 0 ]]; then
+    echo "# Install failed, aborting... #"
+    exit 1
+  fi
 
-    cp main.sh kf1_functions.sh /home/steam/servers/kf/System
+  echo "#### Copying run-time functions to /System ####"
+  cp main.sh kf1_functions.sh /home/steam/servers/kf/System
 fi
 
 cd /home/steam/servers/kf/System
