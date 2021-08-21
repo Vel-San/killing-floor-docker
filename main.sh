9dc47bc2d8f1fd8750900cbb454da29ed84f8223
Vel <13340023+Vel-San@users.noreply.github.com>
2021-08-21 20:35:22 +0200

    [misc] Added echos to track script progress

--- main.sh
+++ main.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 # Source functions below
+echo "#### Sourcing KF1_Functions.sh ####"
 source kf1_functions.sh
 
 # Generates .ini
+echo "#### Running require_config() ####"
 require_config
 # Optimize KF
+echo "#### Running optimize_kf() ####"
 optimize_kf
 # Loads custom config from vars
+echo "#### Running load_config() ####"
 load_config
 # Start Server
+echo "#### Running launch() ####"
 launch
